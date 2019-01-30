6038b51cacaa06f7959fd81620a0b8007d10a4d7
visualstudio <visualstudio@122-PC32>
2019-01-30 11:24:53 -0500

    nodes

--- C0727466-WEEK-04/Program.cs
+++ C0727466-WEEK-04/Program.cs
@@ -18,7 +18,33 @@ namespace C0727466_WEEK_04
         //Node is an ADT
         //What KIND of DATA do we need in a nood?
         public Village nextVillage;
+        public Village previousVillage;
         public string VillageName;
         public bool isAstrildeHere = false;
+    }
+    class Countryside
+    {
+        Village Maple = new Village();
+        Village Toronto = new Village();
+        Village Ajax = new  Village();
+        Village First;
+        Village Last;
+
+        public void  launch()
+        {
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousVillage = Maple;
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.nextVillage = Toronto;
+
+
+        }
+
+
     }
 }
